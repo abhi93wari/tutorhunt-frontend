fd28face3c99e7988f05d2a559665d3c4eae668f
thesandx <kumarjha.sandeep@gmail.com>
2021-02-11 02:29:25 +0530

    material ui

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,181 +1,127 @@
 
 import React, { Component} from 'react';
-import { Form} from 'react-bootstrap';
+import { CardColumns, Form} from 'react-bootstrap';
 import {connect} from "react-redux";
 import Fuse from 'fuse.js';
-import "./dashboard.css"
+
 import axios from 'axios'
-import {Button,Grid,AppBar,Toolbar,IconButton,Typography} from '@material-ui/core'
+import {Button,Grid,AppBar,Toolbar,IconButton,Typography,Container, CardContent,Card,CardMedia} from '@material-ui/core'
 import {MenuIcon} from '@material-ui/icons'
+import { makeStyles ,useTheme} from '@material-ui/core/styles';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+
+
+
 
 let characters = []
 
 
+const useStyles = makeStyles((theme) =>({
+  root: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 151,
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+  playIcon: {
+    height: 38,
+    width: 38,
+  },
+}));
 
 
-class dashboard extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-          query: "",
-          cardInfo:[]
-        };
-    }
-
-    componentWillMount(){
-      window.scrollTo(0,0);
-      //this.fetchSubjets();  
-    }
 
 
 
+function Dashboard(){
+
+
+  const classes = useStyles();
+  const theme = useTheme();
+
+
+  return (
+    <>
    
+    <Grid container
+  direction="row"
+  justify="center"
+  alignItems="center"
+>
 
-    fetchSubjets = async (e)=>{
-
-    
-
-      //hit the api
-      let result = await axios.get("http://localhost:8086/allcourses");
-      let res = result.data;
-      console.log("data is "+res);
-      characters = res;
-    }
-
-    handleSearch = (event)=>{
-      this.setState({query: event.target.value});
-    }
-
-    handleChange = async (event)=>{
-
-      this.setState({query: event.target.value});
-    
-      const fuse = new Fuse(characters, {
-        includeScore:true
-      });
-      
-      const result  = fuse.search(this.state.query);
-      console.log(result);
-
-      let str;
-      for(let i in result){
-
-        if(typeof(str)=='undefined'){
-
-          str = result[i];
-
-        }
-
-        else if(result[i].score>str.score){
-          str = result[i];
-        }
-
-      }
-
-      let json  = {
-        "course":" "
-      }
-      
-      //now hit the api
-      if(typeof(str)=='undefined'){
-        console.log("no match found"); 
-      }
-      else{
-        console.log("search keyword is "+str.item);
-        json = {
-          "course":str.item
-        }
-      }
-
-      //hit the api and retrive cardinfo
-
-       //hit the api
-       
-       let result1 = await axios.post("http://localhost:8086/tutorlist",json);
-       let res = result1.data;
-       console.log("data is "+JSON.stringify(res));
-
-
-      // this.state.cardInfo= [
-      //   {
-      //     title:"title1",
-      //     text:"text1",
-      //     price:"$25/hour"
-      //   },
-      //   {
-      //     title:"title2",
-      //     text:"text2",
-      //     price:"$24/hour"
-      //   },
-      //   {
-      //     title:"title2",
-      //     text:"text2",
-      //     price:"$22/hour"
-      //   },{
-      //     title:"title1",
-      //     text:"text1",
-      //     price:"$25/hour"
-      //   }
-      // ]
-
-      this.state.cardInfo = res;
-      this.setState();
-    }
-    
-
-    render(){
+      <Grid item xs={2}>
 
       
 
-      let showEmpty = ()=>{
-        return(
-          <h2>No courses found</h2>
-        )
-      }
+      </Grid>
 
-      const renderCard=(card,index) => {
+      <Grid item xs = {8}>
 
-        return(
 
-          <Grid container direction='column'>
-
-            <Grid item >
-
-            <AppBar position="static">
-              <Toolbar>
-                <Typography variant="h6">
-                  News
-                </Typography>
-                <Button color="inherit">Login</Button>
-              </Toolbar>
-            </AppBar>
-
-            </Grid>
-
-            <Grid item >
-
-            </Grid>
-
-          </Grid>
-
-          
-        
-        );
-      };
+      <Card className={classes.root}>
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography component="h5" variant="h5">
+            Live From Space
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            Mac Miller
+          </Typography>
+        </CardContent>
+        <div className={classes.controls}>
+          <IconButton aria-label="previous">
+            {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
+          </IconButton>
+          <IconButton aria-label="play/pause">
+            <PlayArrowIcon className={classes.playIcon} />
+          </IconButton>
+          <IconButton aria-label="next">
+            {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
+          </IconButton>
+        </div>
+      </div>
+      <CardMedia
+        className={classes.cover}
+        image="/static/images/cards/live-from-space.jpg"
+        title="Live from space album cover"
+      />
+    </Card>
 
 
 
-    return (
-      <>
+      </Grid>
+
+      <Grid item xs={2}>
+
+      
+
+      </Grid>
+
+      
+    </Grid>
 
 
+    </>
+  );
+  
 
-
-
-        </>
-      );
-    }
 }
+
 
 
 const mapStateToProps = (state)=> {
@@ -187,4 +133,4 @@ const mapStateToProps = (state)=> {
     "username":state.username
 }
 }
-export default connect(mapStateToProps)(dashboard);
+export default connect(mapStateToProps)(Dashboard);
